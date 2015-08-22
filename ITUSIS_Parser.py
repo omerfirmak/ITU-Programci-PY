6bce57b4d551bca5e4abd0fa2832bd3e44480c31
Ömer Faruk Irmak <irmako@itu.edu.tr>
2015-08-22 16:03:36 +0300

    ++

--- ITUSIS_Parser.py
+++ ITUSIS_Parser.py
@@ -5,17 +5,16 @@ import re
 from bs4 import BeautifulSoup
 
 class ITUSIS_Parser:
+    days=['Pa','Sa','Ça','Pe','Cu']
     def __init__(self):
         try:
             os.remove('classdb.sqlite')
-            print('Deleted already existing DB')
         except:
-            print('Creating DB')
-
+            pass
         self.db=sqlite3.connect('classdb.sqlite')
         self.cur=self.db.cursor()
         self.cur.execute('''CREATE TABLE classes
-        (CRN TEXT, Code TEXT, Title TEXT, Inst TEXT ,Build TEXT, ClassTime TEXT, Restr TEXT )''')
+        (CRN TEXT, Code TEXT, Title TEXT, Inst TEXT ,Build TEXT, ClassTime TEXT,Restr TEXT )''')
 
 
     def getDepartmentCodes(self):
@@ -32,20 +31,35 @@ class ITUSIS_Parser:
         for dep in self.getDepartmentCodes():
             classList=[]
             classEntry=[]
+            cellEntry=[]
             html = requests.get('http://www.sis.itu.edu.tr/tr/ders_programlari/LSprogramlar/prg.php?fb='+dep).text
             soup = BeautifulSoup(html,'html.parser')
             for rows in soup.findAll('tr', {'onmouseover': True , 'onmouseout': True}):
                 for cells in rows.findAll('td'):
                     for elem in cells.descendants:
                         if(isinstance(elem,str)):
-                            classEntry.append(elem)
-                            break
+                            cellEntry.append(elem)
+                    classEntry.append(cellEntry)
+                    cellEntry=[]
                 classList.append(classEntry)
                 classEntry=[]
             self.addToDatabase(classList)
 
     def addToDatabase(self,data):
         for classEntry in data:
-            self.cur.execute("INSERT INTO classes VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}')".format(classEntry[0],classEntry[1],
-            classEntry[2].replace('\'',''),classEntry[3],classEntry[4],classEntry[6],classEntry[11]))
+            insert_query="INSERT INTO classes VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}')".format(classEntry[0][0],classEntry[1][0],
+            classEntry[2][0].replace('\'',''),classEntry[3][0],classEntry[4][0],self.calcClassTime(classEntry[5],classEntry[6]),classEntry[11][0])
+            self.cur.execute(insert_query)
         self.db.commit()
+
+    def calcClassTime(self,day,hour):
+        list =[]
+        if day[0][0] == '-':
+            return "Undefined"
+
+        for i in range(0,len(day)):
+            hourasd = hour[i].split('/')
+            start_t = ITUSIS_Parser.days.index(day[i][:2])*14+(int(hourasd[0][:2])-8);
+            stop_t = start_t + int(hourasd[1][:2]) - int(hourasd[0][:2])
+            list.append(str(start_t) +'-' + str(stop_t))
+        return ','.join(list)
--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from ITUSIS_Parser import ITUSIS_Parser
 import time
 start_time = time.time()
 parser = ITUSIS_Parser()
-print(parser.getClasses())
+parser.getClasses()
 print("--- %s seconds ---" % (time.time() - start_time))
