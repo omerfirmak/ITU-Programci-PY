fe72c09e79475295464296e597acb3b0d6778175
Ömer Faruk Irmak <irmako@itu.edu.tr>
2015-08-22 16:06:11 +0300

    ==

--- main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-from ITUSIS_Parser import ITUSIS_Parser
-import time
-start_time = time.time()
-parser = ITUSIS_Parser()
-parser.getClasses()
-print("--- %s seconds ---" % (time.time() - start_time))
