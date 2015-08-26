8a7d3ccd4b878c59d26a81cdfe7f6a4b02cecff3
Ömer Faruk Irmak <irmako@itu.edu.tr>
2015-08-26 23:26:07 +0300

    ++

--- qt_main.py
+++ qt_main.py
@@ -14,14 +14,14 @@ class ITU_Programci():
     def __init__(self):
         self.databaseUpdateUnderway=False
 
-        self.programci = QtWidgets.QApplication(sys.argv)
+        self.app = QtWidgets.QApplication(sys.argv)
         self.ui = ui.Ui_MainWindow()
         self.ui.show()
         self.initDbConnection()
         self.initDepCodeComboBoxes()
         self.connectHandlers()
 
-        sys.exit(self.programci.exec_())
+        sys.exit(self.app.exec_())
         return
 
     def initDbConnection(self):
@@ -42,7 +42,6 @@ class ITU_Programci():
 
     def initDepCodeComboBoxes(self):
         if self.databaseUpdateUnderway:
-            print('db update')
             return
         depCodeList=['']
         for code in self.db.execute('select distinct Depcode from classes'):
@@ -61,15 +60,49 @@ class ITU_Programci():
                 obj.currentIndexChanged.connect(handlers[j])
 
     def depCodeSelectedHandler(self):
-        print('depCodeSelectedHandler')
-        pass
+        if self.databaseUpdateUnderway:
+            return
+
+        senderComboBox = self.ui.sender()
+        index = senderComboBox.objectName().split('_')[1]
+        depCode = senderComboBox.currentText()
+
+        nextComboBox = self.ui.findChild(QtWidgets.QComboBox,'classCodeComboBox_%s' % index)
+
+        if depCode == '':
+            nextComboBox.clear()
+            return
+
+        self.db.execute('select distinct Code from classes where Depcode=\'%s\'' % depCode)
+        classCodeList = ['']
+        for elem in self.db.fetchall():
+            classCodeList.append(elem[0])
+
+        nextComboBox.clear()
+        nextComboBox.addItems(classCodeList)
 
     def classCodeSelectedHandler(self):
-        print('classCodeSelectedHandler')
-        pass
+        if self.databaseUpdateUnderway:
+            return
+
+        senderComboBox = self.ui.sender()
+        index = senderComboBox.objectName().split('_')[1]
+        classCode = senderComboBox.currentText()
+        nextComboBox = self.ui.findChild(QtWidgets.QComboBox,'availClassComboBox_%s' % index)
+
+        if classCode == '':
+            nextComboBox.clear()
+            return
+
+        self.db.execute('select CRN , Title , Inst  ,Build , Day ,Time  from classes where Code=\'%s\'' % classCode)
+        availClassList =['']
+        for elem in self.db.fetchall():
+            availClassList.append('%s :: %s :: %s :: %s :: %s :: %s' % (elem[0],elem[1],elem[2],elem[4],elem[5],elem[3]))
+
+        nextComboBox.clear()
+        nextComboBox.addItems(availClassList)
 
     def classSelectedHandler(self):
-        print('classSelectedHandler')
         pass
 
 
